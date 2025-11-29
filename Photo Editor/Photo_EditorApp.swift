20378e2fbb5fce2bfdc1ed3a1675c77475464950
Bill <bill092804@gmail.com>
2025-11-29 20:53:47 +0800

    Initial Commit

---
+++ Photo Editor/Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorApp.swift
+//  Photo Editor
+//
+//  Created by 陳芸萱 on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
